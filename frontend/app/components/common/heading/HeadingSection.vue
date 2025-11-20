4d96ecd26b428e46aad97a7ea39cea0285d0ffd4
Borodin Dmitriy <borodin.dmitriy@dit-service.ru>
2025-11-20 12:48:50 +0300

    [ add ] - models carousel

---
+++ frontend/app/components/common/heading/HeadingSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    level?: number
+    bottomSpace?: boolean
+  }>(),
+  {
+    level: 1,
+    bottomSpace: false,
+  },
+)
+const component = computed(() => `h${props.level}` as keyof HTMLElementTagNameMap)
+</script>
+
+<template>
+  <component
+    :is="component"
+    :class="{ 'pb-10': props.bottomSpace }"
+    class="text-[40px] leading-12 font-bold"
+  >
+    <slot />
+  </component>
+</template>
