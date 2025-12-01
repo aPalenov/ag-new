20b90565cb089faf5cac17fe502c1817fed51dfb
Borodin Dmitriy <borodin.dmitriy@dit-service.ru>
2025-12-01 12:47:16 +0300

    [ add ] - hover effects for preview

---
+++ frontend/app/components/common/PreviewImage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { NuxtLink } from '#components'
+
+const props = defineProps<{
+  preview?: string
+  title: string
+  to?: string
+  bgClass?: string
+}>()
+
+const component = props.to ? NuxtLink : 'div'
+</script>
+
+<template>
+  <component
+    :is="component"
+    :to="props.to"
+    class="relative w-full overflow-hidden"
+    :class="props.bgClass || 'bg-black/80'"
+  >
+    <img
+      v-if="props.preview"
+      :src="props.preview"
+      :alt="props.title"
+      class="absolute inset-0 h-full w-full object-cover transition-transform duration-500 group-hover:scale-105"
+    />
+    <slot />
+  </component>
+</template>
--- frontend/app/components/videos/cards/VideoCardDefault.vue
+++ frontend/app/components/videos/cards/VideoCardDefault.vue
@@ -10,24 +10,24 @@ const props = defineProps<{
 <template>
   <div class="group flex h-full w-full flex-col overflow-hidden rounded-t-3xl">
     <!-- Картинка / заглушка -->
-    <div class="relative mb-7 overflow-hidden bg-black/80 pt-[66.6%]">
-      <img
-        v-if="props.preview"
-        :src="props.preview"
-        :alt="props.title"
-        class="absolute inset-0 h-full w-full object-cover transition duration-500 group-hover:scale-105"
-      />
-
+    <PreviewImage
+      class="mb-7 aspect-3/2"
+      :to="props.url"
+      :preview="props.preview"
+      :title="props.title"
+    >
       <div class="absolute inset-0 flex items-center justify-center">
         <div class="drop-shadow-lg">
           <UIcon name="ag:play-solid" class="text-4xl text-white" />
         </div>
       </div>
-    </div>
+    </PreviewImage>
 
     <!--  -->
     <h3 class="mb-5 text-xl leading-6 font-bold">
-      {{ props.title }}
+      <NuxtLink :to="props.url" class="hover:text-brand-accent transition-colors">
+        {{ props.title }}
+      </NuxtLink>
     </h3>
 
     <!--  -->
